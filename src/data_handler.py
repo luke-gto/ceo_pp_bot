8be5fb1197d9fed494d42e8852b26868b6f33500
luca-go <54832122+luca-go@users.noreply.github.com>
2022-04-29 10:48:42 +0200

    better formatting

--- src/data_handler.py
+++ src/data_handler.py
@@ -2,26 +2,27 @@ import pandas as pd
 from src.database_handler import insert
 from datetime import date
 
+
 def get_poll_data(date_session, poll):
 
-    sep = ' - Il '
+    sep = " - Il "
     poll_dict = poll.__dict__
-    poll_json = poll_dict['json']['poll']
-    question = poll_json['question'] ### 
-    
+    poll_json = poll_dict["json"]["poll"]
+    question = poll_json["question"]  ###
+
     presenter = question.split(sep, 1)[0]
     category = "il_" + question.split(sep, 1)[1]
-    total_voters = poll_json['total_voter_count'] ###risposta al comando save
+    total_voters = poll_json["total_voter_count"]  ###risposta al comando save
     answers = {}
-                                                        #Nel dataframe finale: presenter, category, final_score
-    for item in poll_json['options']:
-        name = int(item['text'])
+    # Nel dataframe finale: presenter, category, final_score
+    for item in poll_json["options"]:
+        name = int(item["text"])
         answers[name] = item
 
     vote_dataframe = pd.DataFrame.from_dict(answers).T
 
     vote_dataframe = vote_dataframe.astype(int)
-    final_score_cursato = vote_dataframe.prod(axis=1) #idk why but it works
+    final_score_cursato = vote_dataframe.prod(axis=1)  # idk why but it works
     final_score = final_score_cursato.sum()
 
     insert(date_session, presenter, category, int(final_score))
--- src/database_handler.py
+++ src/database_handler.py
@@ -4,24 +4,43 @@ import os
 import pandas as pd
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
-db_path = os.path.dirname(os.path.normpath(script_directory)) + "/data/score_presentazioni.db"
+db_path = (
+    os.path.dirname(os.path.normpath(script_directory)) + "/data/score_presentazioni.db"
+)
+
 
 def create_table(date_session):
     connection = sqlite3.connect(db_path, check_same_thread=False)
     cursor = connection.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS [' + date_session + '] (presenter TEXT PRIMARY KEY, il_ridere INTEGER, il_sapere INTEGER, il_sacrificio INTEGER, combo INTEGER)')
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS ["
+        + date_session
+        + "] (presenter TEXT PRIMARY KEY, il_ridere INTEGER, il_sapere INTEGER, il_sacrificio INTEGER, combo INTEGER)"
+    )
     connection.commit()
     cursor.close()
     connection.close()
 
+
 def insert(date_session, presenter, category, final_score):
     connection = sqlite3.connect(db_path, check_same_thread=False)
     cursor = connection.cursor()
-    cursor.execute("INSERT or IGNORE INTO [" + date_session + "] (presenter," + category + ") VALUES(?, ?)", (presenter, final_score))
-    cursor.execute("UPDATE [" + date_session + "] SET " + category + "= (?) WHERE presenter=(?)", (final_score, presenter))
+    cursor.execute(
+        "INSERT or IGNORE INTO ["
+        + date_session
+        + "] (presenter,"
+        + category
+        + ") VALUES(?, ?)",
+        (presenter, final_score),
+    )
+    cursor.execute(
+        "UPDATE [" + date_session + "] SET " + category + "= (?) WHERE presenter=(?)",
+        (final_score, presenter),
+    )
     connection.commit()
     cursor.close()
     connection.close()
+
 
 def get_combo_score(date_session):  ### risultato classifica generale
     connection = sqlite3.connect(db_path, check_same_thread=False)
@@ -30,38 +49,58 @@ def get_combo_score(date_session):  ### risultato classifica generale
     connection.commit()
     cursor.close()
     connection.close()
-    
 
-def query_data(date_session):               ### TODO: fix spaghetti code
+
+def query_data(date_session):  ### TODO: fix spaghetti code
     get_combo_score(date_session)
     connection = sqlite3.connect(db_path, check_same_thread=False)
     cursor = connection.cursor()
 
-    cursor.execute('SELECT presenter, il_ridere FROM [' + date_session + '] ORDER BY il_ridere DESC')
+    cursor.execute(
+        "SELECT presenter, il_ridere FROM ["
+        + date_session
+        + "] ORDER BY il_ridere DESC"
+    )
     il_ridere_list = cursor.fetchall()
     il_ridere_classification = []
     for i in range(len(il_ridere_list)):
         name = il_ridere_list[i][0]
         score = il_ridere_list[i][1]
-        il_ridere_classification.append(str(i + 1) + ". " + str(name) + " = " + str(score))
-    
-    cursor.execute('SELECT presenter, il_sapere FROM [' + date_session + '] ORDER BY il_sapere DESC')
+        il_ridere_classification.append(
+            str(i + 1) + ". " + str(name) + " = " + str(score)
+        )
+
+    cursor.execute(
+        "SELECT presenter, il_sapere FROM ["
+        + date_session
+        + "] ORDER BY il_sapere DESC"
+    )
     il_sapere_list = cursor.fetchall()
     il_sapere_classification = []
     for i in range(len(il_sapere_list)):
         name = il_sapere_list[i][0]
         score = il_sapere_list[i][1]
-        il_sapere_classification.append(str(i + 1) + ". " + str(name) + " = " + str(score))
+        il_sapere_classification.append(
+            str(i + 1) + ". " + str(name) + " = " + str(score)
+        )
 
-    cursor.execute('SELECT presenter, il_sacrificio FROM [' + date_session + '] ORDER BY il_sacrificio DESC')
+    cursor.execute(
+        "SELECT presenter, il_sacrificio FROM ["
+        + date_session
+        + "] ORDER BY il_sacrificio DESC"
+    )
     il_sacrificio_list = cursor.fetchall()
     il_sacrificio_classification = []
     for i in range(len(il_sacrificio_list)):
         name = il_sacrificio_list[i][0]
         score = il_sacrificio_list[i][1]
-        il_sacrificio_classification.append(str(i + 1) + ". " + str(name) + " = " + str(score))
+        il_sacrificio_classification.append(
+            str(i + 1) + ". " + str(name) + " = " + str(score)
+        )
 
-    cursor.execute('SELECT presenter, combo FROM [' + date_session + '] ORDER BY combo DESC')
+    cursor.execute(
+        "SELECT presenter, combo FROM [" + date_session + "] ORDER BY combo DESC"
+    )
     combo_list = cursor.fetchall()
     combo_classification = []
     for i in range(len(combo_list)):
@@ -69,7 +108,13 @@ def query_data(date_session):               ### TODO: fix spaghetti code
         score = combo_list[i][1]
         combo_classification.append(str(i + 1) + ". " + str(name) + " = " + str(score))
 
-    return(combo_classification, il_ridere_classification, il_sapere_classification, il_sacrificio_classification)
+    return (
+        combo_classification,
+        il_ridere_classification,
+        il_sapere_classification,
+        il_sacrificio_classification,
+    )
+
 
 def get_table_list():
     connection = sqlite3.connect(db_path, check_same_thread=False)
@@ -81,4 +126,3 @@ def get_table_list():
     cursor.close()
     connection.close()
     return list
-
